e7c1e6774ee33b493f6a86b5460944a927629e4c
gfaruk02 <gfaruk02@gmail.com>
2023-10-18 18:47:59 +0600

    add middleware cors and express.json

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
+// middleware
+app.use(cors());
+app.use(express.json());
 
 
 
